7c5a6b7caabb9e286f674a88bc194f0cf5251e25
Vitalii <vitamarys.web@neogara.com>
2023-02-08 12:24:40 +0200

    fix bug with overflow

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const burgerBtn = document.querySelector('.bugrer');
 const asside = document.querySelector('.aside');
-
+const body = document.querySelector('body');
 
 burgerBtn.addEventListener('click',()=>{
   asside.classList.toggle('active')
+  body.classList.toggle('active')
 })
